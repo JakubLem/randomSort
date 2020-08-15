103c9fd02e010693c9569829f659e5b94ae62986
Jakub Lemiesiewicz <jakub.lemiesiewicz@gmail.com>
2020-08-15 11:10:43 +0200

    list init

--- project/main.cpp
+++ project/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
+class List{
+
+private:
+
+    vector<int> list;
+
+public:
+
+    List() {}
+
+    int len() {
+        return this->list.size();
+    }
+
+    void append(int n) {
+        this->list.push_back(n);
+    }
+
+    ~List() {}
+};
+
 int main() {
-    cout << "test ";
     return 0;
 }
