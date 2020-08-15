e679832de6620f021287254b35d7907cedb9345f
Jakub Lemiesiewicz <jakub.lemiesiewicz@gmail.com>
2020-08-15 11:39:40 +0200

    overload operator <<

--- project/main.cpp
+++ project/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <ostream>
 using namespace std;
 
 class List{
@@ -19,10 +20,26 @@ public:
     void append(int n) {
         this->list.push_back(n);
     }
+    friend ostream& operator<<(ostream& os, const List& obj);
+    
 
     ~List() {}
 };
 
+ostream& operator<<(ostream& os, const List& obj)
+{
+    os << " ( ";
+    for(auto elem: obj.list){
+        os << elem << ", ";
+    }
+    os << " ) ";
+    return os;
+}
+
 int main() {
+    List tab = List();
+    tab.append(5);
+    tab.append(4);
+    cout << tab;
     return 0;
 }
